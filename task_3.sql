9e3cc36b96c90cf4e71ab36d7925b36a60d5d881
Joseph Hillary Enyinnaya <156797451+Hillaryjhizy@users.noreply.github.com>
2024-08-06 15:50:14 +0100

    Add files via upload

---
+++ task_3.sql
@@ -0,0 +1,43 @@
+
+import mysql.connector
+from mysql.connector import Error
+
+def create_server_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host= "localhost",
+            user=u "root",
+            passwd= "S0lomon_Busayo@1997",
+            database= "lx_book_store"
+        )
+        print("MySQL Server connection successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+def list_tables(connection):
+    cursor = connection.cursor()
+    try:
+        cursor.execute("SHOW TABLES")
+        tables = cursor.fetchall()
+        if tables:
+            print("Tables in the database:")
+            for table in tables:
+                print(table[0])
+        else:
+            print("No tables found in the database.")
+    except Error as err:
+        print(f"The error '{err}' occurred")
+
+
+# Connect to MySQL Server
+connection = create_server_connection(host_name, user_name, user_password, db_name)
+
+# List all tables
+if connection:
+    list_tables(connection)
+    connection.close()
+    print("MySQL connection is closed")
+else:
+    print("Failed to connect to the database.")
